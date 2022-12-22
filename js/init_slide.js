a660d376ab0e28a9a4e1665f2919d3ef779af45d
Ethan-Brantley <51418822+Ethan-Brantley@users.noreply.github.com>
2022-12-22 11:06:18 -0700

    Portfolio

---
+++ js/init_slide.js
@@ -0,0 +1,38 @@
+const swiper = new Swiper('.swiper', {
+    // Optional parameters
+    direction: 'horizontal',
+    loop: true,
+    slidesPerView: 3,
+    spaceBetween: 30,
+    centeredSlides: true,
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+
+    pagination: {
+        el: ".swiper-pagination",
+        dynamicBullets: true,
+      },
+
+    breakpoints: {
+        "@0.00": {
+          slidesPerView: 1.5,
+          spaceBetween: 30,
+        },
+        "@0.75": {
+          slidesPerView: 1.5,
+          spaceBetween: 50,
+        },
+        "@1.00": {
+          slidesPerView: 3,
+          spaceBetween: 100,
+        },
+        "@1.50": {
+          slidesPerView: 2.5,
+          spaceBetween: 100,
+        },
+      },
+  });
