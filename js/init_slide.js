9c35345cc2679c9981f969d11c0009de5aca5fa9
Ethan-Brantley <51418822+Ethan-Brantley@users.noreply.github.com>
2023-02-10 12:49:22 -0700

    services carousel and background adjustments

--- js/init_slide.js
+++ js/init_slide.js
@@ -34,3 +34,40 @@ const swiper = new Swiper('.swiper', {
         },
       },
   });
+
+const services = new Swiper('.services', {
+    // Optional parameters
+    direction: 'horizontal',
+    loop: true,
+    centeredSlides: true,
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+
+    pagination: {
+        el: ".swiper-pagination",
+        dynamicBullets: true,
+      },
+
+    breakpoints: {
+        "@0.00": {
+          slidesPerView: 1,
+          spaceBetween: 30,
+        },
+        "@0.75": {
+          slidesPerView: 1.8,
+          spaceBetween: 50,
+        },
+        "@1.00": {
+          slidesPerView: 1.8,
+          spaceBetween: 50,
+        },
+        "@1.50": {
+          slidesPerView: 3,
+          spaceBetween: 50,
+        },
+      },
+  });
